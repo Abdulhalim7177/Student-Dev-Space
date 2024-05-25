7a375a18d4990fd6c4c755db508d2a1911520576
Abdulhalim Muhammad Yaro <123118801+Abdulhalim7177@users.noreply.github.com>
2024-05-25 16:00:15 +0100

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,50 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+(function ($) {
+
+    "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click', function () {
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function () {
+        $('.hero-slides').vegas({
+            slides: [
+                { src: 'images/slides/Foc.jpg'},
+                { src: 'images/slides/buk2'},
+                { src: 'images/slides/buk1.jgp'}
+            ],
+            timer: false,
+            animation: 'kenburns',
+        });
+    });
+
+    // CUSTOM LINK
+    $('.smoothscroll').click(function () {
+        var el = $(this).attr('href');
+        var elWrapped = $(el);
+        var header_height = $('.navbar').height() + 60;
+
+        scrollToDiv(elWrapped, header_height);
+        return false;
+
+        function scrollToDiv(element, navheight) {
+            var offset = element.offset();
+            var offsetTop = offset.top;
+            var totalScroll = offsetTop - navheight;
+
+            $('body,html').animate({
+                scrollTop: totalScroll
+            }, 300);
+        }
+    });
+
+})(window.jQuery);
+
+
